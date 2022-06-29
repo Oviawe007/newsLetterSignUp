ae8ebd71fce2a5e09000a7fa043726d4da4bf018
oviawe007 <josephoviawe4@gmail.com>
2022-06-29 17:41:50 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+const PORT = 3000;
+
+app.use(express.static("public"))
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/", (req,res) => {
+    res.sendFile(__dirname + "/signup.html")
+});
+
+
+app.post("/failure", function(req,res){
+    res.redirect("/")
+});
+
+app.post("/" , (req,res)=>{
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    const firstName = req.body.firstName;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields:{
+                    FNAME: firstName,
+                    LNAME:lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us8.api.mailchimp.com/3.0/lists/3dd44af3b5"
+
+    const options = {
+        method: "POST",
+        auth: "skyloft:f0869481757f99bde4cf7ce3132a437c-us8"
+    }
+
+
+   const request = https.request(url, options, function(response){
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html")
+        } else{
+            res.sendFile(__dirname + "/failure.html")
+        }
+        response.on("data" , function(data){
+            console.log(JSON.parse(data))
+        })
+
+    });
+
+    request.write(jsonData);
+    request.end();
+    //console.log(firstName +"\n "+ lastName +"\n "+ email)
+
+});
+
+
+//API KEY
+// f0869481757f99bde4cf7ce3132a437c-us8
+
+//list_id
+// 3dd44af3b5
+
+
+
+
+
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log("Server Started on port "+ PORT)
+});
